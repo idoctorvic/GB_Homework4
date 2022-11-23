9ca7d4ae46621e30f1876314c16a6f4ac2c7fe32
idoctorvic <loukianova@yahoo.com>
2022-11-23 19:58:20 +0300

    Task27 Summ of digits

---
+++ Task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int digitSum(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        int digit = num % 10;
+        sum += digit;
+        num = num / 10;
+    }
+    return sum;
+}
+
+int res1 = digitSum(452);
+Console.WriteLine(res1);
+
+int res2 = digitSum(82);
+Console.WriteLine(res2);
+
+int res3 = digitSum(9012);
+Console.WriteLine(res3);
